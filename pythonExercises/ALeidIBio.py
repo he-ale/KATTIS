117fbe76f18f6d8301dfb72459cbdc396c217589
Manuel Alejandro Cladera Mamani <he.ale.cladera@gmail.com>
2026-08-31 21:17:58 -0400

    083120262117

---
+++ pythonExercises/ALeidIBio.py
@@ -0,0 +1,11 @@
+
+def solve(a: int, b: int, c:int):
+    return c - (a + b)
+
+
+if __name__ == "__main__":
+    a= int(input())
+    b= int(input())
+    c= int(input())
+
+    print(solve(a, b, c))
